88730c644e62d3a382243a59fb3dd54371a2fe8d
valerieduffey <69978259+valerieduffey@users.noreply.github.com>
2020-09-04 13:22:03 -0400

    Delete index.java

--- index.java
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>CSCE 190: Valerie Duffey</title>
-    </head>
-    <body>
-        <hi> Valerie Duffey's CSCE 190 Sit </h1>e
-    </body>
-</html>
